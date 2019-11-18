320618eda4f7431b157b2b9d8ebd50f18b198f17
Angelo Marzari Possatti <angelopossatti@gmail.com>
2019-11-17 22:44:11 -0300

    Add store function to UserController. Create user admin with seeds.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
+
+routes.post('/users', UserController.store);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Gym Point!' });
