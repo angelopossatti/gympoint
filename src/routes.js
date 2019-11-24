0c4188e7c0460f51440ef6475cc9ea15300d1cc7
Angelo Marzari Possatti <angelopossatti@gmail.com>
2019-11-24 20:56:40 -0300

    Add a middleware auth to users.

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,18 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.post('/session', SessionController.store);
+routes.post('/sessions', SessionController.store);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Gym Point!' });
 });
+
+routes.use(authMiddleware); // just for below routes.
+routes.put('/users', UserController.update);
 
 export default routes;
