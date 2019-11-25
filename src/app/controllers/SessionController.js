d8b23a4c76dfde4ca625d4ab575c147fc9c88d0f
Angelo Marzari Possatti <angelopossatti@gmail.com>
2019-11-24 21:52:50 -0300

    Add Yup validation schema.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,22 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação dos campos.' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -14,7 +26,7 @@ class SessionController {
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Senha não encontrada.' });
+      return res.status(401).json({ error: 'Senha incorreta.' });
     }
 
     const { id, name } = user;
